a4a49e55a46e0596ba82fc198d64672e2caf1a62
Tareq AbuSaleh <boj10506@bankofjordan.com.jo>
2023-02-09 14:25:35 +0300

    Tareq

---
+++ app/getcardidimages/src/main/java/com/bankofjordan/getcardidimages/MainActivityLib.java
@@ -0,0 +1,68 @@
+package com.bankofjordan.getcardidimages;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+
+public class MainActivityLib extends AppCompatActivity {
+    private final int CAMERA_REQ = 101;
+    private final int WRITE_REQ = 102;
+    private final int READ_REQ = 103;
+    private IdViewModel viewModel;
+    Function mCallback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_lib);
+        viewModel = new ViewModelProvider(this).get(IdViewModel.class);
+        Navigation.findNavController((Activity) this, R.id.nav_host_fragment);
+        permissions();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (!viewModel.getFrontFile().getValue().isBlank() || !viewModel.getFrontFile().getValue().isEmpty()) {
+            new File(viewModel.getFrontFile().getValue()).delete();
+        }
+        if (!viewModel.getFrontFile().getValue().isBlank() || !viewModel.getFrontFile().getValue().isEmpty()) {
+            new File(viewModel.getFrontFile().getValue()).delete();
+        }
+    }
+
+    public void initGetCard(Function callBack) {
+        mCallback = callBack;
+        Intent intent = new Intent(getApplicationContext(), getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.setFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        startActivity(intent);
+    }
+
+    //********************************Check Permission*****************************//
+    private final void permissions() {
+        this.checkPermissions("android.permission.CAMERA", "camera", this.CAMERA_REQ);
+        this.checkPermissions("android.permission.WRITE_EXTERNAL_STORAGE", "write external storage", this.WRITE_REQ);
+        this.checkPermissions("android.permission.READ_EXTERNAL_STORAGE", "read external storage", this.READ_REQ);
+    }
+
+    private final void checkPermissions(String permission, String name, int reqCode) {
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this.getApplicationContext(), permission) != 0) {
+            ActivityCompat.requestPermissions((Activity) this, new String[]{permission}, reqCode);
+        }
+    }
+}
--- app/src/main/java/com/bankofjordan/getcardid/MainActivity.java
+++ app/src/main/java/com/bankofjordan/getcardid/MainActivity.java
@@ -1,9 +1,12 @@
 package com.bankofjordan.getcardid;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
+
 import com.bankofjordan.getcardidimages.MainActivityLib;
 
 import java.util.function.Function;
@@ -17,13 +20,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(v -> {
-            Intent myIntent = new Intent(MainActivity.this, MainActivityLib.class);
-            startActivity(myIntent);
+            MainActivityLib mm = new MainActivityLib();
+            mm.initGetCard(test());
         });
     }
 
+    public Function test() {
+        Toast.makeText(this, "Hello from main", Toast.LENGTH_SHORT).show();
+
+        return null;
+    }
 
 }
